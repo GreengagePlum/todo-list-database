3bcff99c5660be397b6b870743b8bbb1ab1acd6b
Efe ERKEN <efeerkenn@hotmail.com>
2024-01-07 19:56:17 +0000

    :wrench: UPDATE: Add explanation comments
    Added two lines of comments in the main DBeaver script to notify about
    its workings.

--- sql/main.sql
+++ sql/main.sql
@@ -1,6 +1,8 @@
+-- La création des tables, vues, procédures, fonctions, contraintes, index et déclancheurs
 @include ddl/suppression.sql
 @include ddl/creation.sql
 
+-- L'insertion des données de test
 @include dml/score.sql
 @include dml/comporte.sql
 @include dml/utilisateur.sql
